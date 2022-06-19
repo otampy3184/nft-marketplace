32db26c52ff5d5d204599f3d79d5dcfb6fda8307
django_freeman <o.tampodo.t.18@gmail.com>
2022-06-19 18:38:13 +0900

    Mint動作確認完了

--- src/contracts/ERC721Connector.sol
+++ src/contracts/ERC721Connector.sol
@@ -7,10 +7,6 @@ import './ERC721.sol';
 // ERC721Metadataを外部に繋げるコントラクト
 contract ERC721Connector is ERC721Metadata, ERC721 {
 
-    string [] public kryptoBirdz;
-    mapping(string => bool) _kryptoBirdzExists;
-
-
     // ERC721Connectorをデプロイした際にMetadataを渡す
     constructor(string memory name, string memory symbol) ERC721Metadata(name, symbol) {
 
