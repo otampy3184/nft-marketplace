7e6b1c19ba00bdb4008a396d148b055ffff31b11
django_freeman <o.tampodo.t.18@gmail.com>
2022-06-19 18:29:04 +0900

    主コントラクトにミント機能を追加

--- src/contracts/ERC721Connector.sol
+++ src/contracts/ERC721Connector.sol
@@ -2,17 +2,14 @@
 pragma solidity ^0.8.0;
 
 import './ERC721Metadata.sol';
+import './ERC721.sol';
 
 // ERC721Metadataを外部に繋げるコントラクト
-contract ERC721Connector is ERC721Metadata {
+contract ERC721Connector is ERC721Metadata, ERC721 {
 
     string [] public kryptoBirdz;
     mapping(string => bool) _kryptoBirdzExists;
 
-    // Mint用
-    function mint(string memory _kryptoBird) public {
-        require(!_kryptoBirdzExists[_kryptoBird], 'ERROR: kryptoBird already exists');
-    }
 
     // ERC721Connectorをデプロイした際にMetadataを渡す
     constructor(string memory name, string memory symbol) ERC721Metadata(name, symbol) {
